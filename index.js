0f421c7e067a45bee60de4d0d220b72ac3734ced
Alexandru Varzari <alex.varzari2000@gmail.com>
2023-06-10 19:37:33 +0200

    added and tested function playRound

--- index.js
+++ index.js
@@ -3,4 +3,32 @@ function getComputerChoice() {
   let comptuerChoice = Math.floor(Math.random() * computerInput.length);
   return computerInput[comptuerChoice];
 }
-console.log(getComputerChoice());
+
+function playRound(playerSelection, comptuerSlection) {
+  if (playerSelection === comptuerSlection) {
+    return "This is a Draw!";
+  }
+  if (playerSelection === "scissors" && comptuerSlection === "rock") {
+    return "You lose! rock beats scissors";
+  }
+  if (playerSelection === "rock" && comptuerSlection === "paper") {
+    return "You lose! paper beats rock";
+  }
+  if (playerSelection === "paper" && comptuerSlection === "scissors") {
+    return "You lose! scissors beats paper";
+  }
+
+  if (playerSelection === "rock" && comptuerSlection === "scissors") {
+    return "You win! rock beats scissors";
+  }
+  if (playerSelection === "paper" && comptuerSlection === "rock") {
+    return "You win! paper beats rock";
+  }
+  if (playerSelection === "scissors" && comptuerSlection === "paper") {
+    return "You win! scissors beats paper";
+  }
+}
+
+const playerSelection = "";
+const comptuerSlection = getComputerChoice();
+console.log(playRound(playerSelection, comptuerSlection));
