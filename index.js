146234cf99491d9d049409d3431a0e09febdcb3a
Alexandru Varzari <alex.varzari2000@gmail.com>
2023-06-17 20:16:00 +0200

    created the logic for player, index.js

--- index.js
+++ index.js
@@ -1,38 +1,31 @@
-function getComputerChoice() {
-  let computerInput = ["rock", "paper", "scissors"];
-  let comptuerChoice = Math.floor(Math.random() * computerInput.length);
-  return computerInput[comptuerChoice];
-}
+const buttons = document.querySelectorAll("[data-selection]");
+const SELECTIONS = [
+  {
+    name: "rock",
+    emoji: "✊",
+    beats: "scissors",
+  },
+  {
+    name: "paper",
+    emoji: "✋",
+    beats: "rock",
+  },
+  {
+    name: "scissors",
+    emoji: "🖖",
+    beats: "paper",
+  },
+];
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const buttonName = button.dataset.selection;
+    const selection = SELECTIONS.find(
+      (selection) => selection.name === buttonName
+    );
+    makeSelection(selection);
+  });
+});
 
-function playRound(playerSelection, comptuerSlection) {
-  if (playerSelection === comptuerSlection) {
-    return "This is a Draw!";
-  }
-  //Computer wins
-  if (playerSelection === "scissors" && comptuerSlection === "rock") {
-    return "You lose! rock beats scissors";
-  }
-  if (playerSelection === "rock" && comptuerSlection === "paper") {
-    return "You lose! paper beats rock";
-  }
-  if (playerSelection === "paper" && comptuerSlection === "scissors") {
-    return "You lose! scissors beats paper";
-  }
-  // Player wins
-  if (playerSelection === "rock" && comptuerSlection === "scissors") {
-    return "You win! rock beats scissors";
-  }
-  if (playerSelection === "paper" && comptuerSlection === "rock") {
-    return "You win! paper beats rock";
-  }
-  if (playerSelection === "scissors" && comptuerSlection === "paper") {
-    return "You win! scissors beats paper";
-  }
-}
-
-const playerSelection = prompt().toLocaleLowerCase();
-const comptuerSlection = getComputerChoice();
-
-for (let i = 0; i < 5; i++) {
-  console.log(playRound(playerSelection, comptuerSlection));
+function makeSelection(selection) {
+  console.log(selection);
 }
